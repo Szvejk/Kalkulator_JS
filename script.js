b47e3bef48bffa240c197ac992353e6060ecd9c5
Sara Szwejkowska <saraszw@op.pl>
2022-06-20 19:19:15 +0200

    clicking on numbers

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const usun = document.querySelector('.usun')
 const rownosc = document.querySelector('.rownosc')
 const wynikiPoprzednie = document.querySelector('.poprzednie-dzialanie')
 const wynikiAktualne = document.querySelector('.aktualne-dzialanie')
+
+let aktualneDzialanie = ''
+let poprzednieDzialanie = ''
+let operacja = undefined
+
+const zaktualizujWynik = () => {
+    wynikiAktualne.innerText = aktualneDzialanie
+    wynikiPoprzednie.innerText = poprzednieDzialanie
+}
+const dodajLiczbe = (liczba) => {
+    aktualneDzialanie = aktualneDzialanie.toString() + liczba.toString()
+}
+liczby.forEach((liczba) => {
+    liczba.addEventListener('click', () => {
+        dodajLiczbe(liczba.innerText)
+        zaktualizujWynik()
+    })
+});
